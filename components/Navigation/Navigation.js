1ee432bce976fc6a5b5dd64a38eebe5e6c1d4494
pjevic <milanpjevic@icloud.com>
2023-07-10 12:23:20 +0200

    Setting Up Fonts and favicon.ico

---
+++ components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from "react";
+import styles from "./Navigation.module.scss";
+
+function Navigation() {
+  return <div className={styles.test}>Navigation</div>;
+}
+
+export default Navigation;
