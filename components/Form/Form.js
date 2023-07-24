c5bd5685a6b98e883ab1d0797a922ab5c441c3ba
pjevic <milanpjevic@icloud.com>
2023-07-24 14:23:27 +0200

    Form - Functionality

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -4,11 +4,22 @@
 
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { Cousine, Lato } from "next/font/google";
 import { DevTool } from "@hookform/devtools";
 import PhoneInput from "react-phone-input-2";
 
 import styles from "./Form.module.scss";
 import "react-phone-input-2/lib/style.css";
+
+const cousine = Cousine({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 function Form() {
   const form = useForm({
@@ -18,15 +29,22 @@ function Form() {
         lastName: "",
       },
       dob: "",
+      gender: "",
       email: "",
       phone: "",
+      company: {
+        isCompany: "",
+        name: "",
+        id: "",
+        taxID: "",
+      },
     },
     mode: "onTouched",
   });
 
   const [phoneNumber, setPhoneNumber] = useState("");
 
-  const { register, control, handleSubmit, formState, reset, setValue } = form;
+  const { register, control, handleSubmit, formState, reset, watch } = form;
 
   const { errors, isDirty, isSubmitSuccessful } = formState;
 
@@ -41,6 +59,8 @@ function Form() {
     setPhoneNumber(value);
   };
 
+  const isCompany = watch("company.isCompany") === "no";
+
   useEffect(() => {
     if (isSubmitSuccessful) {
       reset();
@@ -49,13 +69,15 @@ function Form() {
   }, [isSubmitSuccessful, reset]);
 
   return (
-    <div>
+    <div style={lato.style}>
       <form
         className={styles.form}
         onSubmit={handleSubmit(onSubmit)}
         noValidate
       >
-        <div className={styles.form__title}>upoznajmo se</div>
+        <div style={cousine.style} className={styles.form__title}>
+          upoznajmo se
+        </div>
 
         <div className={styles.form__name}>
           <div className={styles.form__firstName}>
@@ -127,10 +149,32 @@ function Form() {
         </div>
 
         <div className={styles.form__gender}>
-          <input type="radio" name="gender" id="male" value="male" />
+          <input
+            type="radio"
+            id="male"
+            value="male"
+            {...register("gender", {
+              required: {
+                value: true,
+                message: "Ovo polje je obavezno.",
+              },
+            })}
+          />
           <label htmlFor="male">Male</label>
-          <input type="radio" name="gender" id="female" value="female" />
+
+          <input
+            type="radio"
+            id="female"
+            value="female"
+            {...register("gender", {
+              required: {
+                value: true,
+                message: "Ovo polje je obavezno.",
+              },
+            })}
+          />
           <label htmlFor="female">Female</label>
+          <p className={styles.error}>{errors.gender?.message}</p>
         </div>
 
         <div className={styles.form__email}>
@@ -184,24 +228,81 @@ function Form() {
 
         <div className={styles.form__company}>
           <label htmlFor="yes">Da</label>
-          <input type="radio" name="company" id="yes" value="yes" />
+          <input
+            type="radio"
+            id="yes"
+            value="yes"
+            {...register("company.isCompany", {
+              required: {
+                value: true,
+                message: "Ovaj unos je obavezan.",
+              },
+            })}
+          />
           <label htmlFor="no">Ne</label>
-          <input type="radio" name="company" id="no" value="no" />
+          <input
+            type="radio"
+            id="no"
+            value="no"
+            {...register("company.isCompany", {
+              required: {
+                value: true,
+                message: "Ovaj unos je obavezan.",
+              },
+            })}
+          />
+          <p className={styles.error}>{errors.company?.isCompany?.message}</p>
         </div>
 
         <div className={styles.form__companyName}>
           <label htmlFor="companyName">Naziv firme</label>
-          <input className={styles.form__input} type="text" id="companyName" />
+          <input
+            className={styles.form__input}
+            type="text"
+            id="companyName"
+            {...register("company.name", {
+              required: {
+                value: true,
+                message: "Ovaj podatak je obavezan.",
+              },
+              disabled: isCompany,
+            })}
+          />
+          <p className={styles.error}>{errors.company?.name?.message}</p>
         </div>
 
         <div className={styles.form__companyID}>
           <label htmlFor="companyID">Matični broj</label>
-          <input className={styles.form__input} type="text" id="companyID" />
+          <input
+            className={styles.form__input}
+            type="number"
+            id="companyID"
+            {...register("company.id", {
+              pattern: {
+                value: /^\d{8}$/,
+                message: "MB mora sadržati 8 cifara.",
+              },
+              disabled: isCompany,
+            })}
+          />
+          <p className={styles.error}>{errors.company?.id?.message}</p>
         </div>
 
         <div className={styles.form__companyTAX}>
           <label htmlFor="companyTAX">PIB:</label>
-          <input className={styles.form__input} type="text" id="companyTAX" />
+          <input
+            className={styles.form__input}
+            type="number"
+            id="companyTAX"
+            {...register("company.taxID", {
+              pattern: {
+                value: /^\d{9}$/,
+                message: "PIB mora sadržati 9 cifara.",
+              },
+              disabled: isCompany,
+            })}
+          />
+          <p className={styles.error}>{errors.company?.taxID?.message}</p>
         </div>
 
         <button className={styles.button} disabled={!isDirty}>
