6fdbce2ca4d6b227b2fa2d998ab2f815ceb6de85
pjevic <milanpjevic@icloud.com>
2023-08-03 22:15:04 +0200

    Component - Article

--- app/about/page.js
+++ app/about/page.js
@@ -1,18 +1,32 @@
 /** @format */
 
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 import Hero from "@/components/Hero/Hero";
+import Article from "@/components/Article/Article";
+
+import data from "./data_SR.json";
+import styles from "./page.module.scss";
+
+const { text1, text2 } = data;
 
 function About() {
   return (
-    <div>
+    <div className={styles.about}>
       <Hero
+        style={{ transform: "translateY(0rem)" }}
         teaser="stručnost koja deluje"
         line1="Vaša sigurna ruka"
         line2="za poslovne izazove"
         line3="i mogućnosti"
       />
+      <section className={styles.about__section}>
+        <Article content={text1} />
+
+        <div className={styles.about__imgs}></div>
+      </section>
     </div>
   );
 }
