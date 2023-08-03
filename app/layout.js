8f3af5dc0a78078dd3e3f4acecfa7f136a0af286
pjevic <milanpjevic@icloud.com>
2023-08-03 17:37:52 +0200

    @media: Footer

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
       <body className={inter.style}>
         <Navigation />
         {children}
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import data from "./data-SR.json";
 import styles from "./Footer.module.scss";
@@ -16,6 +17,7 @@ function Footer() {
   return (
     <footer className={styles.footer} style={lato.style}>
       <Image
+        className={styles.footer__waves}
         src="/assets/imgs/waves/waves-footer-1728px.svg"
         width={1728}
         height={761}
@@ -23,105 +25,128 @@ function Footer() {
       />
 
       <div className={styles.footer__body}>
-        <Image
-          className={styles.footer__logo}
-          src="/assets/imgs/logo/KONA-logo-whiteFill.svg"
-          alt="KO-NA logo"
-          width={122}
-          height={36}
-          priority
-        />
+        <div className={styles["footer__body--container"]}>
+          <Image
+            className={styles.footer__logo}
+            src="/assets/imgs/logo/KONA-logo-whiteFill.svg"
+            alt="KO-NA logo"
+            width={122}
+            height={36}
+            priority
+          />
 
-        <div className={styles.footer__company}>KO-NA DOO</div>
-        <div className={styles["footer__company--description"]}>
-          Društvo za špediciju, <br />
-          računovodstveno-knjigovodstvene <br />
-          usluge, poslovne usluge i trgovinu <br />
-          KO-NA DOO Kikinda
-        </div>
-        <div className={styles["footer__company--data"]}>
-          MB: 08505322 <br />
-          PIB: 100509320
-          <br />
-        </div>
-        <div className={styles["footer__company--social"]}>
-          LinkedIn
-          <br />
-          Facebook
-          <br />
-        </div>
+          <div className={styles.footer__company}>{data.name}</div>
 
-        <div className={styles.footer__department1}>Špedicija</div>
-        <div className={styles["footer__department1--address"]}>
-          Oslobođenja 9<br />
-          23300 Kikinda
-        </div>
-        <div className={styles["footer__department1--contact"]}>
-          <div>
-            <div className={styles.footer__line}>
-              <Image
-                className={styles.footer__icons}
-                src="/assets/imgs/icons/phone.svg"
-                width={15}
-                height={15}
-                alt="Background image with moving waves in a gradient from Midnight Blue to Magenta Pink"
-              />
-              <span>+381230123456</span>
-            </div>
-            <div className={styles.footer__line}>
-              <Image
-                className={styles.footer__icons}
-                src="/assets/imgs/icons/email.svg"
-                width={15}
-                height={15}
-                alt="Background image with moving waves in a gradient from Midnight Blue to Magenta Pink"
-              />
-              <span>spedicija@kona.rs</span>
+          <div className={styles["footer__company--description"]}>
+            {data.description}
+          </div>
+
+          <div className={styles["footer__company--data"]}>
+            <div className={styles["footer__company--mb"]}>MB: {data.MB}</div>
+            <div className={styles["footer__company--pib"]}>
+              PIB: {data.PIB}
             </div>
           </div>
-        </div>
 
-        <div className={styles.footer__department2}>Računovodstvo</div>
-        <div className={styles["footer__department2--address"]}>
-          Trg srpskih dobrovoljaca 51/1
-          <br />
-          23300 Kikinda
-        </div>
-        <div className={styles["footer__department2--contact"]}>
-          <div>
-            <div className={styles.footer__line}>
-              <Image
-                className={styles.footer__icons}
-                src="/assets/imgs/icons/phone.svg"
-                width={15}
-                height={15}
-                alt="Background image with moving waves in a gradient from Midnight Blue to Magenta Pink"
-              />
-              <span>+381230123456</span>
+          <div className={styles["footer__company--social"]}>
+            <Link className={styles["footer__company--link"]} href={"/"}>
+              {data.social.linkedIn.title}
+            </Link>
+
+            <Link className={styles["footer__company--link"]} href={"/"}>
+              {data.social.facebook.title}
+            </Link>
+          </div>
+
+          <div className={styles.footer__department1}>
+            {data.department.dep1.name}
+          </div>
+
+          <div className={styles["footer__department1--address"]}>
+            <div className={styles["footer__address--street"]}>
+              {data.department.dep1.address}
             </div>
-            <div className={styles.footer__line}>
-              <Image
-                className={styles.footer__icons}
-                src="/assets/imgs/icons/email.svg"
-                width={15}
-                height={15}
-                alt="Background image with moving waves in a gradient from Midnight Blue to Magenta Pink"
-              />
-              <span>racunovodstvo@kona.rs</span>
+            <div className={styles["footer__address--city"]}>
+              {data.department.dep1.city}
             </div>
           </div>
-        </div>
 
-        <div className={styles.footer__services}>Usluge</div>
-        <ul className={styles["footer__services--list"]}>
-          <li>Carinsko zastupanje</li>
-          <li>Računovodstvene usluge</li>
-          <li>Knjigovodstvene usluge</li>
-          <li>-----------------------</li>
-          <li>Osnivanje firme</li>
-          <li>Obračun PDV-a </li>
-          <li>Obračun zarada</li>
-        </ul>
+          <div className={styles["footer__department1--contact"]}>
+            <div>
+              <div className={styles["footer__contact--phone"]}>
+                <Image
+                  className={styles.footer__icons}
+                  src={data.department.dep1.phoneIcon}
+                  width={15}
+                  height={15}
+                  alt="Phone Icon"
+                />
+                <span>{data.department.dep1.phone}</span>
+              </div>
+
+              <div className={styles.footer__line}>
+                <Image
+                  className={styles.footer__icons}
+                  src={data.department.dep1.emailIcon}
+                  width={15}
+                  height={15}
+                  alt="Email Icon"
+                />
+                <span>{data.department.dep1.email}</span>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.footer__department2}>
+            {data.department.dep2.name}
+          </div>
+          <div className={styles["footer__department2--address"]}>
+            <div className={styles["footer__address--street"]}>
+              {data.department.dep2.address}
+            </div>
+            <div className={styles["footer__address--city"]}>
+              {data.department.dep2.city}
+            </div>
+          </div>
+          <div className={styles["footer__department2--contact"]}>
+            <div>
+              <div className={styles["footer__contact--phone"]}>
+                <Image
+                  className={styles.footer__icons}
+                  src={data.department.dep2.phoneIcon}
+                  width={15}
+                  height={15}
+                  alt="Phone Icon"
+                />
+                <span>{data.department.dep2.phone}</span>
+              </div>
+
+              <div className={styles.footer__line}>
+                <Image
+                  className={styles.footer__icons}
+                  src={data.department.dep2.emailIcon}
+                  width={15}
+                  height={15}
+                  alt="Email Icon"
+                />
+                <span>{data.department.dep2.email}</span>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.footer__services}>{data.services.title}</div>
+          <ul className={styles["footer__services--list"]}>
+            {data.services.services.map((s, i) => (
+              <Link
+                className={styles["footer__services--link"]}
+                key={i}
+                href={s.link}
+              >
+                {s.name}
+              </Link>
+            ))}
+          </ul>
+        </div>
       </div>
     </footer>
   );
