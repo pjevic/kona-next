3482faec705a88d698bc6b4e5df0d2b815d4e15e
pjevic <milanpjevic@icloud.com>
2023-08-08 14:24:21 +0200

    Page: Services

--- app/services/page.js
+++ app/services/page.js
@@ -4,10 +4,26 @@
 
 import React from "react";
 import { useState } from "react";
+import Image from "next/image";
 import Accordion from "@/components/Accordion/Accordion";
-
 import data from "./data-SR.json";
 import styles from "./page.module.scss";
+import { Cousine, DM_Serif_Display, Lato } from "next/font/google";
+
+const cousine = Cousine({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const dmSerifDispay = DM_Serif_Display({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 function Services() {
   const [article, setArtcle] = useState(null);
@@ -20,35 +36,56 @@ function Services() {
 
       <div className={styles["services__container--article"]}>
         <div className={styles["services__article"]}>
-          <div className={styles["services__article--teaser"]}>
+          <div
+            style={cousine.style}
+            className={styles["services__article--teaser"]}
+          >
             {article?.teaser}
           </div>
-          <div className={styles["services__article--heading"]}>
+          <h1
+            style={dmSerifDispay.style}
+            className={styles["services__article--heading"]}
+          >
             {article?.title}
-          </div>
-          <div className={styles["services__article--text"]}>
+          </h1>
+          <div style={lato.style} className={styles["services__article--text"]}>
             {article?.text.map((p, i) => (
-              <div key={i}>
+              <ul key={i} className={styles["services__article--text--list"]}>
                 {typeof p === "string" ? (
                   <p>{p}</p>
                 ) : (
                   p.map((obj, i) => (
                     <div key={i}>
-                      <span>{obj.bold}</span>
-                      <p>{obj.regular}</p>
+                      <li>
+                        {obj.bold} &nbsp;
+                        <p>{obj.regular}</p>
+                      </li>
                     </div>
                   ))
                 )}
-              </div>
+              </ul>
             ))}
           </div>
         </div>
 
-        <div className={styles["services__article-img"]}></div>
+        <div className={styles["services__article-img"]}>
+          {article && (
+            <Image
+              className={styles["services__article-img--style"]}
+              src={article?.imgPath}
+              width={100}
+              height={100}
+              alt={`Ilustracija ${article?.teaser}`}
+            />
+          )}
+        </div>
       </div>
 
-      <div className={styles["services__container--feature"]}>
-        <div className={styles["services__feature"]}></div>
+      <div
+        style={cousine.style}
+        className={styles["services__container--feature"]}
+      >
+        {article?.teaser}
       </div>
     </div>
   );
